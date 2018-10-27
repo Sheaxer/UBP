5cf1fb53becd439973d1e0dbd208908275fde86e
minop <jakub@kovar.co.uk>
2018-10-27 16:26:12 +0200

    implementacia asymetrickeho sifrovania suboru v CryptoUtils

--- src/CryptoUtils.java
+++ src/CryptoUtils.java
@@ -1,10 +1,8 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.*;
+import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -45,6 +43,48 @@ public class CryptoUtils {
 	{
 		return doCrypto(Cipher.ENCRYPT_MODE,key,inputFile,outputFile,null);
 	}
+
+	public static void encryptAsymetric(PublicKey publicKey, File inputFile, File outputFile) throws Exception {
+		try {
+			// vygenerujeme nahodny kluc a salt
+			// metoda getSalt generuje nahodne byty pomocou PRNG
+			// mozeme ju zavolat dva krat, alebo raz a vyziadat si dvojnasobny pocet bytov (treba trochu upravit kod)
+			// v jednom pripade dva krat inicializujeme PRNG a v druhom musime rozdelovat array
+			// salt aj key budeme nakoniec sifrovat pomocou RSA, takze spojit musime tak ci tak
+			// TL;DR: jedno riesenie je pravdepodobne zanedbatelne rychlejsie, takze to spravime bez uprav kodu pre getSalt
+			byte[] salt = getSalt();
+			byte[] randomKey = getSalt();
+
+			byte[] keyAndSalt = new byte[2*SALTSIZE];
+			System.arraycopy(randomKey, 0, keyAndSalt, 0, randomKey.length);
+			System.arraycopy(salt, 0, keyAndSalt, SALTSIZE, salt.length);
+
+			// len pre istotu overime, ci dlzka kluca + saltu je <= dlzka public kluca
+			if(publicKey instanceof RSAPublicKey) {
+				assert SALTSIZE*8*2 <= ((RSAPublicKey) publicKey).getModulus().bitLength();
+			}
+
+			// zasifrujeme kluc + salt pomocou RSA
+			Cipher cipherAsymetric = Cipher.getInstance(ASYMETRIC_ALGORITHM);
+			cipherAsymetric.init(Cipher.ENCRYPT_MODE, publicKey);
+
+			byte[] encryptedBytes = cipherAsymetric.doFinal(keyAndSalt);
+
+			FileOutputStream out = new FileOutputStream(outputFile);
+			out.write(encryptedBytes);
+
+			out.close();
+
+			// zasifrujem vstupny subor pomocou AES a pridam ho za zasifrovany kluc + salt
+			doCrypto(Cipher.ENCRYPT_MODE, new String(randomKey), inputFile, outputFile, salt, true);
+		}
+		catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | BadPaddingException | IllegalBlockSizeException | IOException ex) {
+			throw new Exception ("Error encrypting/decrypting file " + ex.getMessage());
+		}
+		catch (Exception innerEx) {
+			throw new Exception ("Inner exception: " + innerEx.getMessage());
+		}
+	}
 	
 	/*public static void decrypt(String key, File inputFile, File outputFile, byte[] salt) throws Exception
 	{
@@ -60,9 +100,13 @@ public class CryptoUtils {
 		SecretKey secretKey = new SecretKeySpec(tmp.getEncoded(), ALGORITHM);
 		return secretKey;
 	}
-	
-	public  static byte[] doCrypto(int mode,String key, File inputFile, File outputFile, byte[] salt) throws Exception {
-		// TODO Auto-generated method stub
+
+	public static byte[] doCrypto(int mode,String key, File inputFile, File outputFile, byte[] salt) throws Exception {
+		// pretazena funkcie pre kompatibilitu s existujucim kodom
+		return doCrypto(mode, key, inputFile, outputFile, salt, false);
+	}
+
+	public  static byte[] doCrypto(int mode,String key, File inputFile, File outputFile, byte[] salt, boolean appendOutput) throws Exception {
 		try {
 			
 			if(salt == null)
@@ -77,7 +121,7 @@ public class CryptoUtils {
 			
 			byte[] outputBytes = cipher.doFinal(inputBytes);
 			
-			FileOutputStream out = new FileOutputStream(outputFile);
+			FileOutputStream out = new FileOutputStream(outputFile, appendOutput);
 			out.write(outputBytes);
 			
 			
