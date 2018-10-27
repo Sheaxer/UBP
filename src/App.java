8150063fe5d605824b70e02bad03ceafca28f31b
minop <jakub@kovar.co.uk>
2018-10-27 13:41:39 +0200

    kostra pre bonusovu app

---
+++ src/App.java
@@ -0,0 +1,118 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App extends JFrame implements ActionListener {
+
+    private JRadioButton modeEnc;
+    private JRadioButton modeDec;
+
+    private FileSelector inFileSelector;
+
+    private FileSelector publicKeyFileSelector;
+    private JPanel publicKeyPanel;
+
+    private FileSelector privateKeyFileSelector;
+    private JPanel privateKeyPanel;
+
+    private FileSelector outFileSelector;
+
+    private JButton runBtn;
+
+    public App() {
+        super();
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+
+        // Mode
+        JPanel modePanel = new JPanel();
+        this.add(modePanel);
+        modePanel.add(new JLabel("Mód:"));
+
+        ButtonGroup rbModeGroup = new ButtonGroup();
+
+        this.modeDec = new JRadioButton("Dešifrovanie");
+        rbModeGroup.add(this.modeDec);
+        modePanel.add(this.modeDec);
+        this.modeDec.addActionListener(this);
+        this.modeDec.setSelected(true);
+
+        this.modeEnc = new JRadioButton("Šifrovanie");
+        rbModeGroup.add(this.modeEnc);
+        modePanel.add(this.modeEnc);
+        this.modeEnc.addActionListener(this);
+
+        // Input File
+        JPanel inFilePanel = new JPanel();
+        this.add(inFilePanel);
+        inFilePanel.add(new JLabel("Vstupný súbor:"));
+
+        this.inFileSelector = new FileSelector(false, true);
+        inFilePanel.add(this.inFileSelector);
+
+        // Public key
+        this.publicKeyPanel = new JPanel();
+        this.add(this.publicKeyPanel);
+        this.publicKeyPanel.add(new JLabel("Verejný kľúč (.DER):"));
+
+        this.publicKeyFileSelector = new FileSelector(true, true);
+        this.publicKeyPanel.add(this.publicKeyFileSelector);
+        this.publicKeyPanel.setVisible(false);
+
+        // Private key
+        this.privateKeyPanel = new JPanel();
+        this.add(this.privateKeyPanel);
+        this.privateKeyPanel.add(new JLabel("Súkromný kľúč (.DER):"));
+
+        this.privateKeyFileSelector = new FileSelector(true, true);
+        this.privateKeyPanel.add(this.privateKeyFileSelector);
+
+        // Output file
+        JPanel outFilePanel = new JPanel();
+        this.add(outFilePanel);
+        outFilePanel.add(new JLabel("Výstupný súbor:"));
+
+        this.outFileSelector = new FileSelector(false, false);
+        outFilePanel.add(this.outFileSelector);
+
+        // Button
+        JPanel runBtnPanel = new JPanel();
+        this.add(runBtnPanel);
+        this.runBtn = new JButton("Spustiť");
+        runBtnPanel.add(this.runBtn);
+        this.runBtn.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+
+        if(source.equals(this.modeEnc)) {
+            // show public key field
+            this.publicKeyPanel.setVisible(true);
+            this.privateKeyPanel.setVisible(false);
+        }
+        else if(source.equals(this.modeDec)) {
+            // show private key field
+            this.publicKeyPanel.setVisible(false);
+            this.privateKeyPanel.setVisible(true);
+        }
+        else if(source.equals(this.runBtn)) {
+            // do encryption/decryption
+            if(this.modeEnc.isSelected()) {
+                // Encryption
+
+            }
+            else {
+                // Decryption
+
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        App a = new App();
+        a.setSize(640, 260);
+        a.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        a.setVisible(true);
+    }
+}
